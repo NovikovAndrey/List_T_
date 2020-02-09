04a0a20943b5f8fd1ffbf91273370ae0a7a52bf6
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-09 14:50:15 +0300

    Add project files.

---
+++ List_T_/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Separator;
+namespace List_T_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleSeparator consoleSeparator = new ConsoleSeparator('-', 50);
+            SeparateEvent separateEvent = new SeparateEvent();
+            separateEvent.OnSeparate += consoleSeparator.Separator;
+            List<int> numbers = new List<int>() { 1, 2, 3, 45 };
+            numbers.Add(6);
+            numbers.AddRange(new int[] { 7, 8, 9 });
+            numbers.Insert(0, 666);
+            numbers.RemoveAt(1);
+            foreach(int x in numbers)
+            {
+                Console.WriteLine(x);
+            }
+            separateEvent.Display();
+        }
+    }
+}
